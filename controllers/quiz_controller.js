79709e7c93bd0109ba22506317ac802b779388ff
ipalomino <isaac.palomino@hotmail.com>
2015-07-25 12:38:08 +0200

    Se crea el listado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,6 +2,37 @@
 var models = require ('../models/models.js');
 
 
+// GET /users/:userId/quizes
+exports.index = function(req, res) {  
+  models.Quiz.findAll().then(function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes});
+    });
+};
+
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function (quiz){
+  	res.render('quizes/show', { quiz: quiz});
+  });
+}; 
+
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+
+	models.Quiz.find(req.params.quizId).then(function (quiz){
+		if(req.query.respuesta === quiz.respuesta) {
+			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});	
+		} else {
+			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
+		}
+	});
+};
+
+
+
+/*
 exports.question = function(req, res) {
 	models.Quiz.findAll().success(function (quiz){
 		res.render('quizes/question', {pregunta: quiz[0].pregunta})
@@ -25,6 +56,11 @@ exports.answer = function (req, res) {
 	});
 };
 
+*/ 
+
+
 exports.author = function (req, res) {
 	res.render('author/author', {nombre: 'Isaac Palomino', email: 'isaac.palomino@hotmail.com'});
 };
+
+
