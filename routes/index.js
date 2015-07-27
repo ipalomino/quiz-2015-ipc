155463aed3122b5dd063f5e6bbee5445560049d7
unknown <ipalomino@ITEM-85029.dhcp.mano.es.sopra>
2015-07-27 19:56:50 +0200

    se añade el buscador de preguntas

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.param('quizId', quizController.load);
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 /*
 router.get('/quizes/question', quizController.question);
